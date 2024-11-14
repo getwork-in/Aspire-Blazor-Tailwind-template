4c9748ff98e1adb96a79341833fd918b8926e5c3
philou94 <lafrance.philippe94@hotmail.com>
2024-11-13 22:40:32 -0500

    Add landing layout

--- BlazorApp7/BlazorApp7.Client/Utils/Class.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlazorApp7.Client.Utils;
-
-public static class Utils
-{
-	public static string CombineClasses(params string[] classes)
-	{
-		return string.Join(" ", classes.Where(c => !string.IsNullOrWhiteSpace(c)));
-	}
-}
